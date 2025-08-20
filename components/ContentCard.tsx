c891a31169a389959b51d35e235726596dde9bf9
letsgo999 <letsgo999@gmail.com>
2025-08-20 16:48:36 +0900

    Update ContentCard.tsx

--- components/ContentCard.tsx
+++ components/ContentCard.tsx
@@ -1,3 +1,4 @@
+
 import React, { useState, useRef, useEffect } from 'react';
 import type { ContentItem } from '../types';
 import { PLATFORM_DETAILS, DEFAULT_PLATFORM_DETAILS } from '../constants';
