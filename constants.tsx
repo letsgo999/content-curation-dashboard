f5126516469271020161cc94ce312133c8e523aa
letsgo999 <letsgo999@gmail.com>
2025-08-21 14:16:53 +0900

    Update constants.tsx

--- constants.tsx
+++ constants.tsx
@@ -32,6 +32,19 @@ export const NewsArticleIcon = ({ className }: { className?: string }) => (
     </svg>
 );
 
+export const GenericIcon = ({ className }: { className?: string }) => (
+    <svg className={className} xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+        <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-1-13h2v6h-2zm0 8h2v2h-2z"/>
+    </svg>
+);
+
+export const DEFAULT_PLATFORM_DETAILS = {
+    name: '기타',
+    Icon: GenericIcon,
+    color: 'text-gray-700',
+    bgColor: 'bg-gray-200',
+    borderColor: 'border-gray-300',
+};
 
 export const PLATFORM_DETAILS = {
   [Platform.YouTube]: {
